748cfeed20f8e1856a7a2c044798e0beac33a7af
William Ahrons <william.ahrons@gmail.com>
2017-10-10 17:02:46 -0300

    Change Color of final alert on install to green

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@ rm $ZSH/themes/pygmalion.zsh-theme
 ln -s $(pwd)/pygmalion.zsh-theme $ZSH/themes/pygmalion.zsh-theme
 mv ~/.zshrc ~/.zshrc.bak
 cat ~/.zshrc.bak | sed 's/ZSH_THEME.*/ZSH_THEME="pygmalion"/' >> ~/.zshrc
-echo Theme successfully installed
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo ${GREEN}Theme successfully installed${NC}
